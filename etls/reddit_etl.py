7a929da324798c13856510f486a077785ca943d5
mlmsmith <mlmsmith@hotmail.co.uk>
2025-02-09 20:02:37 +0000

    Added extract_post function

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -12,3 +12,11 @@ def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     except Exception as e:
         print(e)
         sys.exit(1)
+
+def extract_post(reddit_instance: Reddit, subreddit: str, time_filter:str, limit=None):
+    subreddit = reddit_instance.subreddit(subreddit)
+    posts = subreddit.top(time_filter=time_filter, limit=limit)
+
+    post_lists = []
+    print(posts)
+    #for post in posts:
